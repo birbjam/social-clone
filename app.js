483a3133d82be36292a144b3f9f7cc52566c06de
birbjam <41635757+birbjam@users.noreply.github.com>
2020-01-09 10:39:17 -0800

    set up mvc pattern, router file, start user controller

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
-
 const app = express()
+const router = require('./router')
+
 let port = process.env.PORT || 3000
+
+// Setting up code to accept two of the most common ways of submitting data
+// Traditional HTML form submit
+app.use(express.urlencoded({ extended: false }))
+// JSON data submit
+app.use(express.json())
 
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.render('home-guest')
-})
+app.use('/', router)
 
 app.listen(port)
