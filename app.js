f0de30ccce0d5f9ff3e49153d2a89d813a70926d
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-02-08 14:26:42 -0800

    add create post, edit routes, include sample env file

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ let sessionOptions = session({
 app.use(sessionOptions)
 app.use(flash())
 
+app.use(function(req, res, next) {
+  res.locals.user = req.session.user
+  next()
+})
+
 // Setting up code to accept two of the most common ways of submitting data
 // Traditional HTML form submit
 app.use(express.urlencoded({ extended: false }))
